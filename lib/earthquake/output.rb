c1e66df98d68866e8e4ef7767bed584d58923b03
jugyo <jugyo.org@gmail.com>
2011-03-17 13:00:08 +0900

    show in_reply_to_status_id

--- lib/earthquake/output.rb
+++ lib/earthquake/output.rb
@@ -6,7 +6,8 @@ module Earthquake
       insert do
         while item = item_queue.shift
           begin
-            puts "[#{item["id"]}] #{item["user"]["screen_name"]}: #{item["text"]}"
+            puts "[#{item["id"]}] #{item["user"]["screen_name"]}: #{item["text"]}" +
+                  (item["in_reply_to_status_id"] ? " (reply to #{item["in_reply_to_status_id"]})" : "")
           rescue => e
             ap item
           end
