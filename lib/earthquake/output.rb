fd71c6b0daa86a1a205084a650a26eda158a69f2
jugyo <jugyo.org@gmail.com>
2011-03-17 14:21:29 +0900

    refactor

--- lib/earthquake/output.rb
+++ lib/earthquake/output.rb
@@ -2,17 +2,19 @@ module Earthquake
   module Output
     def output
       return if item_queue.empty?
-
       insert do
         while item = item_queue.shift
-          begin
-            puts "[#{item["id"]}] #{item["user"]["screen_name"]}: #{item["text"]}" +
-                  (item["in_reply_to_status_id"] ? " (reply to #{item["in_reply_to_status_id"]})" : "")
-          rescue => e
-            ap item
-          end
+          puts_item(item)
         end
       end
+    end
+
+    def puts_item(item)
+      puts "[#{item["id"]}] #{item["user"]["screen_name"]}: #{item["text"]}" +
+            (item["in_reply_to_status_id"] ? " (reply to #{item["in_reply_to_status_id"]})" : "")
+    rescue => e
+      puts e
+      ap item
     end
 
     def insert(*messages)
