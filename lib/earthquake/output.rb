764009950a1fd22ac1752a535cecaa76b5eec224
jugyo <jugyo.org@gmail.com>
2011-03-18 16:24:16 +0900

    いろいろ

--- lib/earthquake/output.rb
+++ lib/earthquake/output.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 module Earthquake
   module Output
     def output
@@ -11,10 +12,12 @@ module Earthquake
 
     def puts_items(items)
       [items].flatten.each do |item|
-        begin
-          output_handers.each { |p| p.call(item) }
-        rescue => e
-          puts e, e.backtrace
+        output_handers.each do |p|
+          begin
+            p.call(item)
+          rescue => e
+            puts e, e.backtrace
+          end
         end
       end
     end
@@ -40,33 +43,48 @@ module Earthquake
     end
 
     def color_of(screen_name)
-      config[:colors][screen_name.to_i(36) % config[:colors].size]
+      colors[screen_name.to_i(36) % colors.size]
+    end
+
+    def colors
+      config[:colors]
     end
   end
 
   init do
     output_handers.clear
 
-    config[:colors] = (31..36).to_a + (91..96).to_a
+    config[:colors] ||= (31..36).to_a + (91..96).to_a
 
     output_hander do |item|
-      if item["text"]
-        misc = (item["in_reply_to_status_id"] ? " (reply to #{item["in_reply_to_status_id"]})" : "")
-        user_color = color_of(item["user"]["screen_name"])
-        text = item["text"].e.gsub(/[@#]([0-9A-Za-z_]+)/) do |i|
-          c = color_of($1)
-          "<#{c}>#{i}</#{c}>"
-        end
-        status = "<90>[#{item["id"].to_s.e}]</90> " +
-                 "<#{user_color}>#{item["user"]["screen_name"].e}</#{user_color}>: " +
-                 "#{text}<90>#{misc.e}</90>"
-        puts status.t
+      next unless item["text"]
+
+      misc = (item["in_reply_to_status_id"] ? " (reply to #{item["in_reply_to_status_id"]})" : "")
+      user_color = color_of(item["user"]["screen_name"])
+      text = item["text"].e.gsub(/[@#]([0-9A-Za-z_]+)/) do |i|
+        c = color_of($1)
+        "<#{c}>#{i}</#{c}>"
       end
+      status = "<90>[#{item["id"].to_s.e}]</90> " +
+               "<#{user_color}>#{item["user"]["screen_name"].e}</#{user_color}>: " +
+               "#{text}<90>#{misc.e}</90>"
+      puts status.t
     end
 
     output_hander do |item|
-      if item["delete"]
+      next unless item["event"]
+
+      case item["event"]
+      when "follow"
+        puts "[follow] #{item["source"]["screen_name"]} => #{item["target"]["screen_name"]}"
+      when "favorite", "unfavorite"
+        puts "[#{item["event"]}] #{item["source"]["screen_name"]} => #{item["target"]["screen_name"]} : #{item["target_object"]["text"]}"
+      when "delete"
         puts "[deleted] #{item["delete"]["status"]["id"]}"
+      else
+        if config[:debug]
+          ap item
+        end
       end
     end
   end
