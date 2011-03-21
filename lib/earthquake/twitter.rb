291ac8c4b54f563b1c2d5acaf8193b73f52ad300
jugyo <jugyo.org@gmail.com>
2011-03-21 23:23:09 +0900

    memoize TwitterOAuth::Client#status

--- lib/earthquake/twitter.rb
+++ lib/earthquake/twitter.rb
@@ -7,5 +7,19 @@ module Earthquake
     @twitter = TwitterOAuth::Client.new(config.slice(:consumer_key, :consumer_secret, :token, :secret))
   end
 
+  once do
+    class ::TwitterOAuth::Client
+      def status_with_cache(id)
+        key = "status:#{id}"
+        unless s = Earthquake.cache.read(key)
+          s = status_without_cache(id)
+          Earthquake.cache.write(key, s, :expires_in => 1.hour.ago)
+        end
+        s
+      end
+      alias_method_chain :status, :cache
+    end
+  end
+
   extend Twitter
 end
