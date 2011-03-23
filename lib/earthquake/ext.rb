fde8441c52211c538370d276f10e9e33874d51ed
jugyo <jugyo.org@gmail.com>
2011-03-23 22:39:25 +0900

    add utility methods

--- lib/earthquake/ext.rb
+++ lib/earthquake/ext.rb
@@ -2,4 +2,28 @@ class String
   def c(*codes)
     "\e[#{codes.join;}m#{self}\e[0m"
   end
+
+  def u
+    gsub(/&(lt|gt|amp|quot|apos);/) do |s|
+      case s
+        when '&amp;' then '&'
+        when '&lt;' then '<'
+        when '&gt;' then '>'
+        when '&apos;' then "'"
+        when '&quot;' then '"'
+      end
+    end
+  end
+
+  def e
+    gsub(/[&<>'"]/) do |s|
+      case s
+        when '&' then '&amp;'
+        when '<' then '&lt;'
+        when '>' then '&gt;'
+        when "'" then '&apos;'
+        when '"' then '&quot;'
+      end
+    end
+  end
 end
