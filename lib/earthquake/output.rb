d554e307a692e592ce931186f45987c28e3e8e25
jugyo <jugyo.org@gmail.com>
2011-05-02 23:50:43 +0900

    Revert "handle hashtags properly"
    This reverts commit 44972238fa73b5c9942418309d75518dbe027be9.
    see also https://github.com/jugyo/earthquake/issues/8

--- lib/earthquake/output.rb
+++ lib/earthquake/output.rb
@@ -96,7 +96,7 @@ module Earthquake
       text = (item["retweeted_status"] && item["truncated"] ? "RT: @#{item["retweeted_status"]["user"]["screen_name"]} #{item["retweeted_status"]["text"]}" : item["text"]).u
       text.gsub!(/\s+/, ' ') unless config[:raw_text]
       text = text.coloring(/@[0-9A-Za-z_]+/) { |i| color_of(i) }
-      text = text.coloring(/(?<![0-9A-Za-z_&?])(#[0-9A-Za-z_]*[A-Za-z_][0-9A-Za-z_]*)/) { |i| color_of(i) }
+      text = text.coloring(/(^#[^\s]+)|(\s+#[^\s]+)/) { |i| color_of(i) }
       text = text.coloring(URI.regexp(["http", "https"]), :url)
 
       if item["_highlights"]
