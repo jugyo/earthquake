9e2db4518eeeee7e153fc423e2004fad4d998c7a
Trevor Bramble <inbox@trevorbramble.com>
2011-05-29 12:12:40 -0700

    Refactoring to make room for additional plugin sources

--- lib/earthquake/plugin.rb
+++ lib/earthquake/plugin.rb
@@ -7,36 +7,49 @@ module Earthquake
     attr_reader :plugin
 
     def allowed_host?( hostname )
-      allowed_hosts = ['gist.github.com']
+      self.allowed_hosts.include? hostname
+    end
 
-      allowed_hosts.include? hostname
+    def allowed_hosts
+      ['gist.github.com']
     end
 
     def plugin_install( plugin_uri )
       uri = URI.parse( plugin_uri )
-      unless uri.host == "gist.github.com"
-        puts "The host must be gist.github.com".c(41)
+
+      if config[:restrict_plugin_source] and !allowed_host? uri.host
+        puts "Installing plugins from unapproved sources disallowed".c(41)
+        puts "Host must be one of: "+self.allowed_hosts.join(', ').c(41)
       else
-        puts "..."
-        return
-        gist_id = uri.path[/\d+/]
-        meta = JSON.parse(open("https://gist.github.com/api/v1/json/#{gist_id}").read)
-        filename = meta["gists"][0]["files"][0]
-        raw = open("https://gist.github.com/raw/#{gist_id}/#{filename}").read
-
-        puts '-' * 80
-        puts raw.c(36)
-        puts '-' * 80
-
-        filename = "#{meta["gists"][0]["repo"]}.rb" if filename =~ /^gistfile/
-        filepath = File.join(config[:plugin_dir], filename)
-        if confirm("Install to '#{filepath}'?")
-          File.open(File.join(config[:plugin_dir], filename), 'w') do |file|
-            file << raw
-            file << "\n# #{m[1]}"
-          end
-          reload
+        case
+          when uri.host == 'gist.github.com'
+            self.plugin_install_gist uri
+          else
+            puts "Unsupported plugin source type".c(41)
         end
+      end
+    end
+
+    private
+
+    def plugin_install_gist( uri )
+      gist_id = uri.path[/\d+/]
+      meta = JSON.parse(open("https://gist.github.com/api/v1/json/#{gist_id}").read)
+      filename = meta["gists"][0]["files"][0]
+      raw = open("https://gist.github.com/raw/#{gist_id}/#{filename}").read
+
+      puts '-' * 80
+      puts raw.c(36)
+      puts '-' * 80
+
+      filename = "#{meta["gists"][0]["repo"]}.rb" if filename =~ /^gistfile/
+      filepath = File.join(config[:plugin_dir], filename)
+      if confirm("Install to '#{filepath}'?")
+        File.open(File.join(config[:plugin_dir], filename), 'w') do |file|
+          file << raw
+          file << "\n# #{m[1]}"
+        end
+        reload
       end
     end
   end
