16d9174773c1a8f484731f3931789e1c995b0bed
Eito Katagiri <eitoball@gmail.com>
2013-01-15 00:05:53 +0900

    upgrade to slop 3.4 and remove workaround for slop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
-require 'rspec'
+# encoding: UTF-8
+require 'rubygems'
+
+require 'bundler'
+Bundler.require
+
+$: << File.expand_path('../../lib', __FILE__)
 require 'earthquake'
 
 RSpec.configure do |config|
